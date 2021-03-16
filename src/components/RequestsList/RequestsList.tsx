bfd18b55272d16ec2f0557842540149666dec84f
Ankirig <qwertzxc0000@gmail.com>
2021-03-16 17:50:11 +0300

    fix: add loading button

--- src/components/RequestsList/RequestsList.tsx
+++ src/components/RequestsList/RequestsList.tsx
@@ -1,20 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './RequestsList.scss';
-import { Button } from '@material-ui/core';
 import { VideoRequest } from '../../models/video';
 import VideoStats from '../VideoStats/VideoStats';
+import withLoading from '../../decorators/withLoading';
+import LoadingButton from '../LoadingButton/LoadingButton';
 
 interface RequestsListProps {
   requestQueue: VideoRequest[];
-  onLoadMore: () => void;
+  onLoadMore: () => Promise<void>;
 }
 
 const RequestsList: FC<RequestsListProps> = ({ requestQueue, onLoadMore }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
     <div className="request-container">
-      <Button className="load-more-button" variant="outlined" color="primary" onClick={onLoadMore}>
+      <LoadingButton
+        className="load-more-button"
+        variant="outlined"
+        color="primary"
+        onClick={withLoading(setIsLoading, onLoadMore)}
+        isLoading={isLoading}
+      >
         Загрузить еще
-      </Button>
+      </LoadingButton>
       {requestQueue.map((video, index) => (
         // eslint-disable-next-line react/no-array-index-key
         <div className="request" key={index}>
